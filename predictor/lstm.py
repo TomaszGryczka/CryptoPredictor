b95e242a3da750c0ed3bfb5592cf2f07463a89de
Tomasz Gryczka <tumek001@gmail.com>
2023-05-21 02:13:28 +0200

    minor refactors

--- predictor/lstm.py
+++ predictor/lstm.py
@@ -11,26 +11,23 @@ class LSTM(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        # LSTM model
+        # create LSTM model
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
                             num_layers=num_layers, batch_first=True, dropout=0.2)
-        self.fc_1 = nn.Linear(hidden_size, 128)
-        self.fc_2 = nn.Linear(128, num_classes)
+        self.first_fully_connected_layer = nn.Linear(hidden_size, 128)
+        self.second_fully_connected_layer = nn.Linear(128, num_classes)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        # initialize hidden state with zeros
-        h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        c_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        # initialize lstm layers
+        initial_hidden_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        initial_cell_state = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
 
-        # propagate input through LSTM
-        # (input, hidden, and internal state)
-        output, (hn, cn) = self.lstm(x, (h_0, c_0))
-
-        # reshaping the data for Dense layer next
-        hn = hn.view(-1, self.hidden_size)
-        out = self.relu(hn)
-        out = self.fc_1(out)
+        # forward propagate lstm
+        _, (final_hidden_state, _) = self.lstm(x, (initial_hidden_state, initial_cell_state))
+        final_hidden_state = final_hidden_state.view(-1, self.hidden_size)
+        out = self.relu(final_hidden_state)
+        out = self.first_fully_connected_layer(out)
         out = self.relu(out)
-        out = self.fc_2(out)
+        out = self.second_fully_connected_layer(out)
         return out
