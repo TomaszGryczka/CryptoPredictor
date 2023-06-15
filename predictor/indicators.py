6d076f51e1afc55b1528e733423d5c2094c1312f
Tomasz Gryczka <tumek001@gmail.com>
2023-06-15 10:50:05 +0200

    add additional indicators

--- predictor/indicators.py
+++ predictor/indicators.py
@@ -5,6 +5,23 @@ def moving_average(data, days):
     df['MA'] = df['Close'].rolling(days).mean()
     return df
 
+def bollinger_bands(data, window=20, num_std=2):
+    df = pd.DataFrame(data)
+
+    df['Rolling Mean'] = df['Close'].rolling(window).mean()
+    df['Standard Deviation'] = df['Close'].rolling(window).std()
+
+    df['Upper Band'] = df['Rolling Mean'] + num_std * df['Standard Deviation']
+    df['Lower Band'] = df['Rolling Mean'] - num_std * df['Standard Deviation']
+
+    df['Bollinger Band Coefficient'] = (df['Close'] - df['Lower Band']) / (df['Upper Band'] - df['Lower Band'])
+
+    df.drop(['Rolling Mean', 'Standard Deviation', 'Upper Band', 'Lower Band'], axis=1, inplace=True)
+
+    df = df.dropna()
+
+    return df
+
 def stochastic_oscillator(data, k_period=14, d_period=3):
     df = pd.DataFrame(data)
 
@@ -30,5 +47,21 @@ def fibonacci_retracement(df):
     df['Fibonacci_0.618'] = high - (diff * 0.618)
     df['Fibonacci_0.764'] = high - (diff * 0.764)
     df['Fibonacci_1.0'] = low
+
+    return df
+
+def difference_indicator(data):
+    df = pd.DataFrame(data)
+    df['Difference Close - Open'] = df['Close'] - df['Open']
+
+    df.drop(['Open'], axis=1, inplace=True)
+
+    return df
+
+def percentage_difference_indicator(data):
+    df = pd.DataFrame(data)
+    df['Percentage Difference Close - Open'] = (df['Close'] - df['Open']) / df['Open'] * 100
+
+    df.drop(['Open'], axis=1, inplace=True)
 
     return df
